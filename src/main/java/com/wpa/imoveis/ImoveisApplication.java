31c759a4529bed4dc66d4735385064fdd8863bb4
Warley Araujo <warley1955@gmail.com>
2025-01-06 21:25:32 -0300

    primeiro commit

---
+++ src/main/java/com/wpa/imoveis/ImoveisApplication.java
@@ -0,0 +1,13 @@
+package com.wpa.imoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wpa/imoveis/ImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpa.imoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
